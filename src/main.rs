11b25819aa0dd8f866f5fdaedb1c8e784ceafb47
Jon Moroney <darakian@gmail.com>
2019-04-09 17:36:34 -0700

    Basic working hyper example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, Write};
+use hyper::Client;
+use hyper::rt::{self, Future, Stream};
+
 fn main() {
-    println!("Hello, world!");
+    rt::run(rt::lazy(|| {
+    let client = Client::new();
+
+    let uri = "http://httpbin.org/ip".parse().unwrap();
+
+    client
+        .get(uri)
+        .map(|res| {
+            println!("Response: {}", res.status());
+        })
+        .map_err(|err| {
+            println!("Error: {}", err);
+        })
+    }));
 }
