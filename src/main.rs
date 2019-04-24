ab572ad905f531f027b054dd12eeb899a5b58d0d
Jon Moroney <darakian@gmail.com>
2019-04-24 14:44:44 -0700

    Basic clap input. No validation of input yet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
 use std::io::{self, Write};
 use hyper::Client;
 use hyper::rt::{self, Future, Stream};
+use clap::{Arg, App};
 
 fn main() {
+    let arguments = App::new("Http Inspection and Fuzzing Interface")
+                        .version("0.0.1")
+                        .author("Jon Moroney jmoroney@hawaii.edu")
+                        .about("Fuzz some endpoints")
+                        .arg(Arg::with_name("urls")
+                               .short("u")
+                               .long("urls")
+                               .value_name("Urls")
+                               .help("Urls to fuzz")
+                               .min_values(1)
+                               .required(true)
+                               .takes_value(true)
+                               .index(1))
+                        .get_matches();
+
+    println!("{:?}", arguments.values_of("urls").unwrap());
+
+
+
+
     rt::run(rt::lazy(|| {
     let client = Client::new();
 
